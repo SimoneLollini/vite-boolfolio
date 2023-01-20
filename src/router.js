1f4040235098d6726a3430ec2679b6a051e12439
SimoneLollini <lollinisimone@gmail.com>
2023-01-20 16:07:00 +0100

    Add no found view page

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from './view/HomeView.vue';
 import ContactView from './view/ContactView.vue';
 import SingleProject from './view/SingleProject.vue';
+import NotFoundView from './view/NoFoundView.vue'
 const router = createRouter({
     history: createWebHistory(),
     routes: [
@@ -18,7 +19,12 @@ const router = createRouter({
             name: 'single-project',
             component: SingleProject,
             path: '/project/:slug',
-        }
+        },
+        {
+            path: '/:pathMatch(.*)*',
+            name: 'not-found',
+            component: NotFoundView
+        },
     ]
 })
 
